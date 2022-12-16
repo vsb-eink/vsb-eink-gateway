48abf59e6a5f19b4cde4f594a3fd00868019cc61
Marek Lukáš <tajnymag@gmail.com>
2022-12-16 11:42:41 +0100

    Changed default port number to 8080

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 import * as env from 'env-var';
 
-export const PORT = env.get('PORT').default('8888').asPortNumber();
+export const PORT = env.get('PORT').default('8080').asPortNumber();
 export const HOST = env.get('HOST').default('0.0.0.0').asString();
